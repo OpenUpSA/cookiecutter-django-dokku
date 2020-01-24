7ca842a784211071c1028cafe4e3677ba83b72a0
JD Bothma <jbothma@gmail.com>
2020-01-24 21:49:50 +0200

    WIP adding default page for end-to-end test and boilerplate

--- tests/test_end_to_end.sh
+++ tests/test_end_to_end.sh
@@ -2,14 +2,15 @@
 set -euf -o pipefail
 
 echo Create test app using this template
-cookiecutter --no-input -o /tmp . project_slug=name-of-the-project
-cd /tmp/name-of-the-project
+cookiecutter --no-input -o /tmp . project_name="End to end test"
+cd /tmp/end_to_end_test
 
 echo Start wait for postgres to come up
 docker-compose up -d db
-bin/wait_for_postgres.sh postgres://name-of-the-project@db/name-of-the-project
+bin/wait_for_postgres.sh postgres://end_to_end_test@db/end_to_end_test
 
 echo Start the app and wait for it
 docker-compose run -d web python manage.py migrate
 docker-compose up -d
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 15 http://localhost:8000
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 5 http://localhost:8000 | \
+     grep "This is the default homepage"
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.Home.as_view(), name="home"),
+]
