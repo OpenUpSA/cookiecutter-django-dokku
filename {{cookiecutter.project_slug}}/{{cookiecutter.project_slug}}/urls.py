2b9c3bdbd79c98920cb50135c72646ecca7f18ef
JD Bothma <jbothma@gmail.com>
2020-01-25 17:07:30 +0200

    Blacken templated app

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -6,9 +6,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
-    path(
-        "{{cookiecutter.app_slug}}",
-        include("{{cookiecutter.project_slug}}.{{cookiecutter.app_slug}}.urls"),
-    ),
+    path("initial_app", include("{{cookiecutter.project_slug}}.{{cookiecutter.app_slug}}.urls"),),
     path("admin/", admin.site.urls),
 ]
