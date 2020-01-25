4ecd6fa6f290be9607dcbaa52f38046270a814ba
JD Bothma <jbothma@gmail.com>
2020-01-25 11:21:25 +0200

    More WIP renaming app

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -4,5 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("{{cookiecutter.app_slug}}.urls")),
 
+    path("{{cookiecutter.app_slug}}", include("{{cookiecutter.app_slug}}.urls")),
+
     path('admin/', admin.site.urls),
 ]
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/views.py
@@ -2,4 +2,4 @@ from django.views import generic
 
 
 class Index(generic.TemplateView):
-    template_name = "{{cookiecutter.app_slug}}/index.html"
+    template_name = "index.html"
