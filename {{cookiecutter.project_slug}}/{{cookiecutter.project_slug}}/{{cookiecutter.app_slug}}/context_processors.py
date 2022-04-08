498f10e72493a5d2ef3b6c45b0585866706d53e7
LucaBazzea <luca.bazzea@gmail.com>
2022-04-08 12:58:10 +0200

    Removed TAG_MANAGER_SCRIPT_NONCE

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.app_slug}}/context_processors.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.app_slug}}/context_processors.py
@@ -1,13 +1,7 @@
-from base64 import b64encode
-from random import randint
-
 from django.conf import settings
 
 
 def tag_manager_id(request):
     return {
-        "TAG_MANAGER_ID": settings.TAG_MANAGER_ID,
-        "TAG_MANAGER_SCRIPT_NONCE": b64encode(
-            str(randint(10 ** 10, 10 ** 11)).encode()
-        ).decode(),
+        "TAG_MANAGER_ID": settings.TAG_MANAGER_ID
     }
